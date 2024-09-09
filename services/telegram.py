d5d548b55f9421d298742ff2ffd82ed00e684661
catgif0 <catgiflover0@gmail.com>
2024-09-09 21:11:38 +0530

    Update telegram.py

--- services/telegram.py
+++ services/telegram.py
@@ -2,8 +2,13 @@ import requests
 import logging
 import os
 
+# Telegram Bot token from environment variable
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+if not TELEGRAM_BOT_TOKEN:
+    logging.error("TELEGRAM_BOT_TOKEN environment variable not set")
+    raise ValueError("TELEGRAM_BOT_TOKEN environment variable not set")
 
+# Function to send a message to Telegram
 def send_telegram_message(message):
     try:
         chat_ids = get_chat_ids()
@@ -23,8 +28,10 @@ def send_telegram_message(message):
     except Exception as e:
         logging.error(f"Failed to send Telegram message: {e}")
 
+# Function to get chat IDs from Telegram
 def get_chat_ids():
     try:
+        logging.info("Fetching updates to identify chat IDs...")
         updates_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates"
         response = requests.get(updates_url)
         if response.status_code != 200:
@@ -32,12 +39,14 @@ def get_chat_ids():
             return []
         data = response.json()
         chat_ids = set()
+
         if 'result' in data:
             for update in data['result']:
                 if 'message' in update or 'channel_post' in update:
                     chat = update.get('message', update.get('channel_post')).get('chat')
                     chat_id = chat['id']
                     chat_type = chat['type']
+                    logging.info(f"Found chat ID: {chat_id} of type {chat_type}. Checking if bot is an admin...")
                     if chat_type == 'private':
                         chat_ids.add(chat_id)
                     else:
